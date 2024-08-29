f66b5bf64eb2434dedd199730d1696e16142e21d
italogabl00 <italogabriel8856@gmail.com>
2024-08-29 17:06:08 -0300

    Feat: ajusting icons position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import { FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-4 text-center text-sm sticky top-[100vh] w-[100vw] bg-inallblack text-inwhite">
-      <div className="container mx-auto flex flex-col items-center">
-        <p>&copy; 2024 - Infinity School, todos os direitos reservados.</p>
-        <div className="flex mt-2 space-x-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <div className="flex-grow text-center">
+          <p>&copy; 2024 - Infinity School, todos os direitos reservados.</p>
+        </div>
+        <div className="flex mr-1 space-x-4">
           <a
             href="https://www.instagram.com/infinity.school/"
             target="_blank"
