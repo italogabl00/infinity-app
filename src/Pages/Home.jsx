7d2896dddeec1c03e846bcffe64c317e63c106f7
italogabl00 <italogabriel8856@gmail.com>
2024-09-09 15:27:29 -0300

    Feat: ajusting in layouts

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,12 +5,12 @@ function Home() {
   return (
     <>
       <div className="relative sm:bg-gradient-to-b from-inred via-20% via-[#e5402e78] to-70%">
-        <div className="bg-inblack text-white min-h-full bg-[url('../../public/imagens/fundo2.jpg')] bg-cover bg-no-repeat bg-opacity-10 flex flex-col items-start justify-start pl-4">
+        <div className="bg-inblack text-white min-h-full bg-[url('../../public/imagens/fundo2.jpg')] bg-cover bg-no-repeat bg-opacity-10 items-start justify-start pl-4 h-[94vh]">
           <div className="relative sm:flex sm:flex-col sm:justify-center sm:items-start sm:px-1 2xl:px-16 4xl:px-28 py-6">
             <Logo />
           </div>
 
-          <div className="sm:mt-0 mt-0 sm:ml-0 ml-32 max-w-lg sm:flex sm:flex-col sm:justify-start sm:items-start sm:max-w-4/5">
+          <div className="sm:mt-0  sm:ml-0 ml-44 max-w-lg sm:flex sm:flex-col sm:justify-start sm:items-start sm:max-w-4/5 flex flex-col gap-7 space-y-3">
             <h3 className="text-5xl tracking-widest sm:tracking-wide sm:text-4xl sml:text-3xl 4xl:mb-[-3vw] lg:mb-[-6vw]">
               CONHEÇA <span className="text-inred">NOSSA</span>
             </h3>
@@ -19,12 +19,12 @@ function Home() {
               EQUIPE
             </h1>
 
-            <p className="mt-4 font-sans sml:text-sm">
+            <p className="mt-4 font-sans sml:text-sm leading-tight">
               Nossa equipe de professores é apaixonada por orientar os alunos em
               seu desenvolvimento criativo.
             </p>
 
-            <p className="mt-4 sml:text-sm">
+            <p className="mt-4 sml:text-sm leading-tight">
               Combinando experiência e dedicação, estamos aqui para inspirar,
               apoiar e capacitar cada aluno em sua jornada de aprendizagem.
               Juntos, cultivamos talentos e preparamos os alunos para alcançarem
@@ -32,7 +32,7 @@ function Home() {
             </p>
 
             <div className="w-full">
-              <div className="mt-10 mb-6   flex flex-row justify-start gap-x-4">
+              <div className="mt-12   flex flex-row justify-start gap-x-4">
                 <Link
                   to="/professores"
                   className="w-3/5 h-10 bg-inred transition hover:bg-indred rounded-3xl flex justify-center items-center text-sm"
